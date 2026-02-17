1cd790a723c625bd0dba611be71235c82f32b9ac
ankitgujre <ankitgujre374@gmail.com>
2026-02-17 18:34:27 +0530

    Intro Array

---
+++ intro.py
@@ -0,0 +1,11 @@
+'''
+1. Array can store only one type of data
+
+2. In Python the size of array is not fixed. Array can increase or decrease their size dynamically.
+
+3. List and Array are not same.
+
+4. Type of Array 
+    a. One Dimensional Array - single row multiple columns
+
+'''
